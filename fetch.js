b6d2692e02af95c60b9f27def0e663a5fcb2712a
abdullahaldys9003 <abdullahaldys9003@gmail.com>
2024-08-16 00:24:54 +0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 
-
+/*
 async function createCategory() {
   try {
     // استخراج البيانات من الكائن الجديد
@@ -28,4 +28,28 @@ async function createCategory() {
     alert(error);
   }
 }
+*/
 
+
+async function createCategory() {
+  try {
+    // استخراج البيانات من الكائن الجديد
+    const name = "alii";
+    const formData = new FormData();
+    // إضافة البيانات إلى نموذج البيانات
+    formData.append('name', name);
+
+    // إرسال طلب POST إلى الملف php
+    await fetch('https://databasepro.infinityfreeapp.com/index.php', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // يمكنك إضافة رسالة نجاح إذا كنت ترغب في ذلك
+    alert('تم إرسال البيانات بنجاح');
+
+  } catch (error) {
+    // إظهار رسالة الخطأ
+    alert('حدث خطأ أثناء إرسال البيانات: ' + error.message);
+  }
+}
